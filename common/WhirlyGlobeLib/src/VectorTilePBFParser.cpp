cf64888ca3a89d36b923af1e4dcf65b1e69d733c
Tim Sylvester <tim@mousebirdconsulting.com>
2020-11-02 15:21:17 -0800

    Try explicit include, add missing source file reference.

--- common/WhirlyGlobeLib/src/VectorTilePBFParser.cpp
+++ common/WhirlyGlobeLib/src/VectorTilePBFParser.cpp
@@ -14,8 +14,9 @@
 #import "WhirlyKitLog.h"
 #import "DictionaryC.h"
 
+#import <../nanopb/pb.h>
+#import <../nanopb/pb_decode.h>
 #import "vector_tile.pb.h"
-#import "pb_decode.h"
 
 #import <vector>
 #import <string>
