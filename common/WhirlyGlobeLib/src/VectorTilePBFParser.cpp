e95417783a2ffc989dc896e4c0a99f6836414f1a
Tim Sylvester <tim@mousebirdconsulting.com>
2020-11-02 15:41:52 -0800

    again!

--- common/WhirlyGlobeLib/src/VectorTilePBFParser.cpp
+++ common/WhirlyGlobeLib/src/VectorTilePBFParser.cpp
@@ -14,8 +14,8 @@
 #import "WhirlyKitLog.h"
 #import "DictionaryC.h"
 
-#import "../nanopb/pb.h"
-#import "../nanopb/pb_decode.h"
+#import "../../local_libs/nanopb/pb.h"
+#import "../../local_libs/nanopb/pb_decode.h"
 #import "vector_tile.pb.h"
 
 #import <vector>
